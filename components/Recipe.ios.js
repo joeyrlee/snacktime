3735f9e170c748ac17c4f0377fa0e5c787f7b314
joeyrlee <jleepianist@gmail.com>
2017-03-08 15:43:33 -0800

    (feat-update) Updated deep-link twitter post (from recipe view) text

--- components/Recipe.ios.js
+++ components/Recipe.ios.js
@@ -106,6 +106,7 @@ class Recipe extends Component {
               <TouchableOpacity onPress={
                 () => this.shareToSocialMedia(
                   `twitter://post?message=I%20just%20made%20${this.props.food.split(' ').join('%20')}%20with%20a%20little%20help%20from%20the%20app%20Snacktime!
+
                   ${this.props.recipe.sourceUrl}`,
                   'Twitter',
                   'https://itunes.apple.com/us/app/twitter/id333903271?mt=8'
