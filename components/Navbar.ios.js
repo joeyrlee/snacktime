3adb18b304b935c259db6d3fb0710dad3d15f55a
joeyrlee <jleepianist@gmail.com>
2017-03-09 16:01:34 -0800

    (refactor) code clean-up; no major changes

--- components/Navbar.ios.js
+++ components/Navbar.ios.js
@@ -36,23 +36,23 @@ export default class Nav extends Component {
 
   render() {
     return (
-        <View style={[styles.navigation]}>
-          <TouchableHighlight style={styles.glyphicon} onPress={this.props.changeToUserPage}>
-            <Image source={{uri: 'https://cdn3.iconfinder.com/data/icons/glypho-free/64/home-128.png'}} style={styles.glyphicon} />
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={styles.glyphicon}
-            onPress={this.props.changeNavigationCamera}
-          >
-            <Image source={{uri: 'https://cdn4.iconfinder.com/data/icons/world-travel-guide/512/travel-05-512.png'}} style={styles.glyphicon} />
-          </TouchableHighlight>
-          <TouchableHighlight
-            style={styles.glyphicon}
-            onPress={() => this.shareToSocialMedia('instagram://camera', 'Instagram', 'https://itunes.apple.com/us/app/instagram/id389801252?mt=8')}
-          >
-            <Image source={{uri: 'https://cdn3.iconfinder.com/data/icons/picons-social/57/78-instagram-512.png'}} style={styles.glyphicon} />
-          </TouchableHighlight>
-        </View>
-      )
+      <View style={[styles.navigation]}>
+        <TouchableHighlight style={styles.glyphicon} onPress={this.props.changeToUserPage}>
+          <Image source={{uri: 'https://cdn3.iconfinder.com/data/icons/glypho-free/64/home-128.png'}} style={styles.glyphicon} />
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.glyphicon}
+          onPress={this.props.changeNavigationCamera}
+        >
+          <Image source={{uri: 'https://cdn4.iconfinder.com/data/icons/world-travel-guide/512/travel-05-512.png'}} style={styles.glyphicon} />
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.glyphicon}
+          onPress={() => this.shareToSocialMedia('instagram://camera', 'Instagram', 'https://itunes.apple.com/us/app/instagram/id389801252?mt=8')}
+        >
+          <Image source={{uri: 'https://cdn3.iconfinder.com/data/icons/picons-social/57/78-instagram-512.png'}} style={styles.glyphicon} />
+        </TouchableHighlight>
+      </View>
+    )
   }
 }
