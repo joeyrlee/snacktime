d45f720de7928ab46bfee68e446df1e918b58dd2
joeyrlee <jleepianist@gmail.com>
2017-03-09 17:27:14 -0800

    (config) Moved apiKeys.js to server/utils

--- server/utils/apiKeys.js
+++ server/utils/apiKeys.js
@@ -1,7 +1,8 @@
 module.exports = {
-  visionKey: 'AIzaSyAA14j-7sIJLDTRZd3bYpZrmCEoFA9IN40',
-  pairingID: 'b5378ca6',
-  pairingKey: '690be2968f8f08b26fcc1f2c9c8f5b90',
-  recipesKey: 'qAjqbB5sPamshJwWJJh01Y3exb3Jp1wBzcOjsnrqegcRf1PCXT',
-  backUpRecipesKey: 'jHbWfZqPEUmsh0NElMAPdMXlfPm1p1M9n5NjsnPD1l0Vjhsjng'
+	visionKey: 'AIzaSyAA14j-7sIJLDTRZd3bYpZrmCEoFA9IN40',
+	pairingID: 'b5378ca6',
+	pairingKey: '690be2968f8f08b26fcc1f2c9c8f5b90',
+	recipesKey: 'qAjqbB5sPamshJwWJJh01Y3exb3Jp1wBzcOjsnrqegcRf1PCXT',
+	backUpRecipesKey: 'jHbWfZqPEUmsh0NElMAPdMXlfPm1p1M9n5NjsnPD1l0Vjhsjng',
+	backUpRecipesKey2: 'OxcC40wXNtmshBb2QuFhuTG8xcUXp1huVw2jsnLmhpBuxYNOI8'
 }
